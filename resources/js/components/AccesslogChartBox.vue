f78ed9b9eb75c7173eda4c8ad58e5183c4866d56
wasuken <wevorence@gmail.com>
2020-01-02 04:58:49 +0900

    fix chartjs -> google-chart.

--- resources/js/components/AccesslogChartBox.vue
+++ resources/js/components/AccesslogChartBox.vue
@@ -1,41 +1,83 @@
 <template>
-	<div>
-		<div>
-			<input name="label_key" type="radio" value="ip_address" id="ip_address" checked
-				   v-on:click="changeKey('ip_address')" />
-			<label for="ip_address">ip_address</label>
+	<div class="box">
+		<div class="controll">
+			<label class="radio" for="ip_address">
+				<input name="label_key" type="radio" value="ip_address" id="ip_address" checked
+					   v-on:click="changeKey('ip_address')" />
+				ip_address
+			</label>
+			<label class="radio" for="user_agent">
+				<input name="label_key" type="radio" value="user_agent" id="user_agent"
+					   v-on:click="changeKey('user_agent')"/>
+				user_agent
+			</label>
+			<label class="radio" for="url">
+				<input name="label_key" type="radio" value="url" id="url"
+					   v-on:click="changeKey('url')"/>url
+			</label>
+			<hr/>
+			<chart
+				:chartType="chartType"
+								:chartData="chartData"
+												:chartOptions="chartOptions"
+			/>
 		</div>
-		<div>
-			<input name="label_key" type="radio" value="user_agent" id="user_agent"
-				   v-on:click="changeKey('user_agent')"/>
-			<label for="user_agent">user_agent</label>
-		</div>
-		<div>
-			<input name="label_key" type="radio" value="url" id="url"
-				   v-on:click="changeKey('url')"/>
-			<label for="url">url</label>
-		</div>
-		<hr/>
-		<chart :labelsKey="labelsKey" ref="chr"></chart>
 	</div>
 </template>
 <script>
- import chart from './AccesslogChart';
+ import Chart from './AccesslogGoogleChart';
  export default{
 	 components: {
-		 chart,
+		 Chart,
 	 },
 	 data: function(){
 		 return{
 			 labelsKey: "ip_address",
+			 chartType: "ColumnChart",
+			 chartData: [],
+			 json: [],
+			 chartOptions: {
+				 title: 'アクセスログ集計',
+				 subtitle: 'accesslogs',
+				 width: '60%',
+				 height: 500,
+			 }
 		 }
 	 },
 	 methods: {
+		 getAccessLogsJson: function(){
+			 let token = document.getElementById('token').value;
+			 fetch('/api/accesslogs?token=' + token)
+				 .then(resp => resp.json())
+				 .then(json => {
+					 this.json = json;
+					 this.parseAccessLogsJsonToData();
+				 });
+		 },
+		 parseAccessLogsJsonToData(){
+			 let ds = {};
+			 this.json.forEach(x => {
+				 if(Number.isInteger(ds[x[this.labelsKey]])){
+					 ds[x[this.labelsKey]]++;
+				 }else{
+					 ds[x[this.labelsKey]] = 1;
+				 }
+			 })
+			 let keys = Object.keys(ds);
+			 keys.sort((x, y) => ds[y] - ds[x]);
+			 let cdata = [[this.labelsKey, 'value']];
+			 keys.slice(0, 10).forEach(x => cdata.push([x, ds[x]]));
+			 console.log(cdata);
+			 this.chartData = cdata;
+		 },
 		 changeKey: function(key){
 			 this.labelsKey = key;
-			 this.$refs.chr.updateKey(key);
+			 this.parseAccessLogsJsonToData();
 		 },
 	 },
+	 mounted: function(){
+		 this.getAccessLogsJson();
+	 }
  }
 
 </script>
