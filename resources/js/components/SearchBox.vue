e24eee17ea3a36e1068a4272634477a803352f80
wasuken <wevorence@gmail.com>
2020-01-19 13:14:43 +0900

    fxxk bots.

---
+++ resources/js/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+	  <input type="text" value="" />
+	</div>
+</template>
+
+<script>
+ export default {
+	 data: function(){
+		 return {
+			 tagsString: "",
+			 tags: [],
+		 };
+	 },
+	 methods: {
+		 clearTags: function(){
+			 this.tagsString = "";
+		 }
+	 },
+	 watch: {
+		 tagsString: function(v){
+			 this.tags = Array.from(new Set(v.split(' ')));
+			 this.tags = this.tags.filter(x => x);
+		 }
+	 }
+ }
+</script>
